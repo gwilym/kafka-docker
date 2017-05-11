96f96d40909edfa5045899bbe706f8d96bcaef0f
Gwilym Evans <gwilym.evans@bigcommerce.com>
2017-05-11 07:31:01 +0000

    map LOG4J_% env vars to entries in log4j.properties

--- start-kafka.sh
+++ start-kafka.sh
@@ -43,12 +43,12 @@ do
   fi
 
   if [[ $VAR =~ ^LOG4J_ ]]; then
-    log4j_name=`echo "$VAR" | tr '[:upper:]' '[:lower:]' | tr _ .`
-    env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
+    log4j_name=`echo "$VAR" | sed -r "s/(LOG4J_.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
+    log4j_env=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
     if egrep -q "(^|^#)$log4j_name=" $KAFKA_HOME/config/log4j.properties; then
-        sed -r -i "s@(^|^#)($log4j_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/log4j.properties #note that no config values may contain an '@' char
+        sed -r -i "s@(^|^#)($log4j_name)=(.*)@\2=${!log4j_env}@g" $KAFKA_HOME/config/log4j.properties #note that no config values may contain an '@' char
     else
-        echo "$log4j_name=${!env_var}" >> $KAFKA_HOME/config/log4j.properties
+        echo "$log4j_name=${!log4j_env}" >> $KAFKA_HOME/config/log4j.properties
     fi
   fi
 done
